7397a8ea0c23f4768740c56400df08865dc80f18
barsbilisim <info@barsbilisim.com.tr>
2014-01-16 08:32:22 -0500

    lang

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => '127.0.0.1',
-			'database'  => 'kdmk',
+			'database'  => 'kyrgyz',
 			'username'  => 'root',
-			'password'  => 'uniserver',
+			'password'  => '20deneme',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -17,9 +17,23 @@
 @yield('content')
 </div>
 <script src="/js/jquery/2.0.3/jquery.min.js"></script>
+
 <script src="/js/bootstrap/3.0.3/bootstrap.min.js"></script>
 @yield('script')
 <a href="#" class="btn btn-primary btn-sm" style="z-index:99999;position: fixed; bottom: 0; right: 0; border-radius: 0; border-top-left-radius: 6px;">{{ trans('messages.go-top') }}</a>
 @include('partial.footer')
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-47056558-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
